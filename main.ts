20f1333bcc7d0b2fbaa7bf9d830c854a1d2a3f63
spaceballtwo <samfwilliams4@icloud.com>
2025-03-10 20:25:08 -0700

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,42 @@
+namespace MultiplayerState {
+    export const score2 = MultiplayerState.create()
+}
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
-    game.gameOver(true)
+    timer.throttle("action", 1000, function () {
+        kcp += 1
+    })
 })
-let mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . 8 8 8 8 . . . . . . . 
-    . . . . . 8 f f 8 . . . . . . . 
-    . . . . . 8 8 8 8 . . . . . . . 
-    . . . . . . 8 8 . . . . . . . . 
-    . . . . . 8 8 8 8 . . . . . . . 
-    . . . . . . 8 8 . . . . . . . . 
-    . . . . . . 8 8 . . . . . . . . 
-    . . . . . 8 8 8 8 . . . . . . . 
-    . . . . . 8 8 8 8 . . . . . . . 
-    . . . . . 8 . . 8 . . . . . . . 
-    `, SpriteKind.Player)
-mySprite = Render.getRenderSpriteVariable()
-tiles.placeOnTile(mySprite, tiles.getTileLocation(6, 15))
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    Render.moveWithController(7, 0, 0)
+})
+controller.A.onEvent(ControllerButtonEvent.Released, function () {
+    Render.moveWithController(5, 2, 0)
+})
+let mySprite = Render.getRenderSpriteVariable()
+tiles.placeOnTile(Render.getRenderSpriteInstance(), tiles.getTileLocation(5, 15))
 scene.cameraFollowSprite(mySprite)
-scene.setBackgroundColor(8)
-tiles.setCurrentTilemap(tilemap`level2`)
-Render.setViewAngleInDegree(240)
+scene.setBackgroundColor(2)
+tiles.setCurrentTilemap(tilemap`level3`)
+Render.setViewAngleInDegree(-90)
+info.setScore(0)
+let kcp = 0
+let lap = 0
 let myCounter = sevenseg.createCounter(SegmentStyle.Thick, SegmentScale.Half, 5)
-myCounter.count += 5
+myCounter.count = 0
+Render.moveWithController(5, 2, 0)
+game.onUpdateInterval(10, function () {
+    info.changeScoreBy(1)
+})
+forever(function () {
+    if (mySprite.tileKindAt(TileDirection.Center, assets.tile`myTile2`)) {
+        if (kcp == 1) {
+            myCounter.count += 1
+            lap += 1
+            pause(1000)
+            kcp = 0
+        }
+    }
+    if (lap == 3) {
+        game.gameOver(true)
+    }
+})
